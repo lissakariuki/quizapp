78fa2b43ed1b1cf2e615e7529ca9db02d6ff793e
lissakariuki <melgaka1106@gmail.com>
2024-01-31 22:50:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+const questions = [
+    { 
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    },
+    {
+        question: "What is Kenya best known for?",
+        answers: [
+            { text: "Tourism", correct: false},
+            { text: "Trade", correct: false},
+            { text: "Sports", correct: true},
+            { text: "None of the above", correct: false},
+        ]
+    },,
+    {
+        question: "Which of the following is not a ministry in Kenya?",
+        answers: [
+            { text: "Sports", correct: false},
+            { text: "Foreign affairs", correct: false},
+            { text: "Education", correct: true},
+            { text: "Banking", correct: false},
+        ]
+    },
+    {
+    question: "Which county is not a city?",
+        answers: [
+            { text: "Kisumu", correct: false},
+            { text: "Machakos", correct: false},
+            { text: "Nairobi", correct: true},
+            { text: "Nakuru", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first president of Kenya?",
+        answers: [
+            { text: "Oginga Odinga", correct: false},
+            { text: "Mwai Kibaki", correct: false},
+            { text: "Jomo Kenyatta", correct: true},
+            { text: "Arap Samoei", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer")
