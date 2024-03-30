d811b6980dad2728a96ac99eadae097314dc7274
lissakariuki <melgaka1106@gmail.com>
2024-03-30 23:22:08 +0300

    updates

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ const questions = [
         answers: [
             { text: "Githeri", correct: false},
             { text: "Mukimo", correct: false},
-            { text: "Mbocho", correct: true},
+            { text: "Mbocho", correct: false},
             { text: "mojito", correct: false},
         ]
     },
