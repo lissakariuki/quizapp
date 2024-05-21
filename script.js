c9fecdd20271f120117c01e7c276135ea49a0f1c
lissakariuki <melgaka1106@gmail.com>
2024-05-21 23:33:33 +0300

    script.js

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ const questions = [
         answers: [
             {text: "2020", correct: true},
             {text: "2022", correct: false},
-            {text: "2023", correct: false},
+            {text: "2025", correct: false},
             {text: "2021",correct: false}, 
         ]
          } //add scores to keep track of the score for each player.
