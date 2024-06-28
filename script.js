44cf5ca30382705f2a146e197cf2a45d401fcd40
lissakariuki <melgaka1106@gmail.com>
2024-06-28 11:10:51 +0300

    script.js

--- script.js
+++ script.js
@@ -220,7 +220,7 @@ const questions = [
     //allow users to type out their preffered name
     //SHOW TOTAL SCORE after the player has finished the test
     //24/06/2024
-    //25/06/24  the final
+    //25/06/24  the final commit since it needs to be hosted soon
 
     //cool designing features to enrich user experience.
     //congratulations pop up when one gets it right
